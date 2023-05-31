ca3a3f57e30db5cb53de46e1fe11b2219fb01a93
yoneshinzato <yone.shinzato@montreal.com.br>
2023-05-30 23:09:36 -0300

    built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1)
+
+  price = 9.99
+
+  rate = 0.67
+
+  title = COURSES[0].description
+
+  course = COURSES[0]
+
 
   onCourseSelected(course: Course) {
     console.log("app component event bubbled", course)
