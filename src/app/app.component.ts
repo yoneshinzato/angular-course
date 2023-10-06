bf4a6508d167e41f9b3361b5c90621f73c5ef1ae
yoneshinzato <chibariyo@gmail.com>
2023-10-06 15:43:54 -0300

    Angular Component @Output - Custom Events and Event Emitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,12 @@ import { CourseCardComponent } from './course-card/course-card.component';
 })
 export class AppComponent {
 
-<<<<<<< HEAD
   coreCourse = COURSES[0];
   rxjsCourse = COURSES[1];
   ngrxCourse = COURSES[2];
-=======
-  courses = COURSES;
-
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
-
-  startDate = new Date(2000, 0, 1)
-
-  price = 9.99
-
-  rate = 0.67
-
-  title = COURSES[0].description
-
-  course = COURSES[0]
-
 
   onCourseSelected(course: Course) {
-    console.log(this.card)
+    console.log('App component - click event bubbled ...', course)
   }
->>>>>>> d0c2886d393d7573810bd9332ffa4b9424bb53b1
 
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
@@ -13,5 +13,14 @@ export class CourseCardComponent {
     required: true
   })
   course: Course;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+
+  onCourseViewed() {
+    console.log('card component - button clicked ...')
+    this.courseSelected.emit(this.course)
+  }
 
 }
